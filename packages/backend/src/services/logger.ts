4d58b055743d38c604ad28070b4c605bf17c91c8
Vladlen Popelenkov <popelenkow@gmail.com>
2026-05-31 02:25:32 +0700

    Use node 26.2.0 (#540)

--- packages/backend/src/services/logger.ts
+++ packages/backend/src/services/logger.ts
@@ -1,5 +1,6 @@
 import { type FastifyLoggerOptions } from 'fastify';
 import { type LoggerOptions, stdSerializers } from 'pino';
+import PinoPretty from 'pino-pretty';
 import { envs } from '../common/envs.js';
 
 export const logger: FastifyLoggerOptions & LoggerOptions = {
@@ -8,13 +9,10 @@ export const logger: FastifyLoggerOptions & LoggerOptions = {
     err: stdSerializers.err,
   },
   errorKey: 'error',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss.l',
-      ignore: 'pid,hostname',
-    },
-  },
+  stream: PinoPretty({
+    colorize: true,
+    translateTime: 'HH:MM:ss.l',
+    ignore: 'pid,hostname',
+  }),
   level: envs.logLevel,
 };
