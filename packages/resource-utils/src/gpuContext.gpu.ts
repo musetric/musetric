9ce1afbb7025d9f2ebff17d28649551360be2d8d
Vladlen Popelenkov <popelenkow@gmail.com>
2026-06-25 14:19:28 +0700

    fix(utils): skip high-performance powerPreference on Windows

--- packages/resource-utils/src/gpuContext.gpu.ts
+++ packages/resource-utils/src/gpuContext.gpu.ts
@@ -4,9 +4,12 @@ export const createGpuContext = async (
   profiling?: boolean,
 ): Promise<GpuContext> => {
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  const adapter = await navigator.gpu?.requestAdapter({
-    powerPreference: 'high-performance',
-  });
+  const adapter = await navigator.gpu?.requestAdapter(
+    // https://issues.chromium.org/issues/369219127
+    navigator.userAgent.includes('Windows')
+      ? {}
+      : { powerPreference: 'high-performance' },
+  );
   if (!adapter) {
     throw new Error('WebGPU adapter not available');
   }
