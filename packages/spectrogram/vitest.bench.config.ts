7665fa512253cb8c6a35cc3fe58268f548025b62
Vladlen Popelenkov <popelenkow@gmail.com>
2026-06-29 04:08:01 +0700

    chore(spectrogram): add bench and vitest test configuration

---
+++ packages/spectrogram/vitest.bench.config.ts
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from 'vitest/config';
+import defaultConfig from './vitest.config.js';
+
+export default defineConfig({
+  test: {
+    ...defaultConfig.test,
+    include: ['**/*.bench.ts'],
+    testTimeout: 0,
+  },
+});
