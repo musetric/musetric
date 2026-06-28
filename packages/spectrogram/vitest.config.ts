590e04ca378f5d58f9d32205fe1522780c2fb65d
Vladlen Popelenkov <popelenkow@gmail.com>
2026-06-29 04:23:12 +0700

    chore(spectrogram): add bench and vitest test configuration (#615)

---
+++ packages/spectrogram/vitest.config.ts
@@ -0,0 +1,30 @@
+﻿import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
+
+const isSkip = process.platform === 'linux';
+
+export default defineConfig({
+  test: {
+    dir: 'src',
+    exclude: isSkip ? ['**/*'] : [],
+    passWithNoTests: isSkip,
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright({
+        launchOptions: {
+          channel: 'chromium',
+          args: [
+            '--enable-unsafe-webgpu',
+            '--disable-webgpu-blocklist',
+            '--ignore-gpu-blocklist',
+          ],
+        },
+        contextOptions: {
+          colorScheme: 'dark',
+        },
+      }),
+      instances: [{ browser: 'chromium' }],
+    },
+  },
+});
